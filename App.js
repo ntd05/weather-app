d7ba5b5a82918ca4a9804017ac3a6b359d44470a
Ntd05 <19020387@vnu.edu.vn>
2022-05-09 17:17:52 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, Text, View, Image, TextInput, StyleSheet,Button } from 'react-native';
+import { useState, useEffect } from 'react';
+import Rain from './assets/rain.png';
+import Search from './assets/search.png';
 
-export default function App() {
+export default function CategoryListItem(props) {
+  const  { name, key } = props;
+
+  const [citySearch, setCitySearch]   = useState('--');
+  const [cityResult, setCityResult]   = useState('Hà Nội');
+  const [ weather, setWeather] = useState({});
+
+  const APP_ID ='bcb394ae008590e4762a2a840458af89';
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityResult}&appid=${APP_ID}&units=metric&lang=vi`;
+
+
+
+  async function getWeather() {
+    const data = await fetch(URL).then(res => res.json());
+    //console.log(data.temp);
+    setWeather([data]);
+  }
+
+  useEffect(() => {
+        getWeather();
+        //console.log(weather);
+    });
+
+  
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.searchContainer}>
+        <TextInput 
+          style ={styles.input} 
+          placeholder="Nhập thành phố" 
+          onChangeText={newCity => setCitySearch(newCity)}></TextInput>
+        <Button
+          style ={styles.textStyle}
+          title='tim'
+          onPress={() => setCityResult(citySearch)}
+        />
+      </View>
+      
+      <Text style ={styles.textStyle} >{weather.name}</Text>
+      <Text style ={styles.textStyle} >{weather.description}</Text>
+      <Image style = {styles.categoryImage} source ={Rain} />
+      <Text style ={styles.textStyle} >{weather.temp}°</Text>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,
+      padding: 16,
+      borderRadius: 4,
+      backgroundColor: '#65cee7',
+      shadowColor: '#000'
   },
+  searchContainer: {
+    display: 'flex',
+  },
+  searchImage: {
+    width: 30,
+    height: 30
+},
+  categoryImage: {
+      width: 100,
+      height: 100
+  },
+  textStyle: {
+      textTransform: 'uppercase',
+      marginTop:30,
+      fontSize: 18,
+      color: 'white'
+
+  },
+  input: {
+    textTransform: 'uppercase',
+    marginTop:30,
+    fontSize: 18,
+    color: 'white',
+    borderBottomColor: 'white'
+
+}
 });
